56504e3fbbc04091754a89c8a09d59d16dd638e2
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2013-11-28 22:51:03 +1000

    Added forked install instrucitons and got into the xmas spirit.

--- ed1/piezo.js
+++ ed1/piezo.js
@@ -1,7 +1,10 @@
 var five = require("johnny-five"),
     board, piezo;
 
-// something odd with piezo library.
+// For this to work, you will need to install a forked version with this command:
+//
+// npm install git://github.com/julianduque/johnny-five.git
+
 
 board = new five.Board({
   debug: true
@@ -20,9 +23,9 @@ board.on("ready", function() {
   });
 
 
-  // piezo.note( volume, duration );
-  piezo.tone( 20, 500 );
-
-  piezo.fade( 0, 20 );
-
+  piezo.song(
+    "e e e e e e e g c d e f f f f f e e e e e d d e d g", 
+    "111113111113111111111511111111111111111111111111231"
+  );
+ 
 });
