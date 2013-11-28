b9484c6f452cbeeb59fb2eb543b783704da8975e
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2013-11-28 21:16:42 +1000

    Accellerometer loops now. Nicer output.

--- ed1/accelerometer.js
+++ ed1/accelerometer.js
@@ -9,10 +9,12 @@ board.on("ready", function() {
     board.firmata.sendI2CConfig(1000);
     board.firmata.sendI2CWriteRequest(0x1D, [0x16, 0x05]); // sets the board mode
 
-    board.firmata.sendI2CWriteRequest(0x1D, [0x06]); // request for X axis, 0x07 is Y and 0x08 is Z
-    board.firmata.sendI2CReadRequest(0x1D, 18, function(data){ // 18 bytes is how many come back...
-        console.log("xaxis: " + data); 
-    });
+    board.loop( 100, function() {
+	    board.firmata.sendI2CWriteRequest(0x1D, [0x06]); // request for X axis, 0x07 is Y and 0x08 is Z
+	    board.firmata.sendI2CReadRequest(0x1D, 18, function(data){ // 18 bytes is how many come back...
+	        console.log("x,y,z:", data[0], data[1], data[2]); 
+	    });
+	});
 
 
   board.repl.inject({
